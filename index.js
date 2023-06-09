c154df58807442c6588b3d7b1c220e262adc9bc1
M Farhan <farhanmahesa10@gmail.com>
2023-06-09 21:01:17 +0700

    add api 3

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const express = require("express");
+// const express = require("express");
+
+// const app = express();
+// const PORT = 4000;
+
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hey this is my API running 🥳");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("This is my about route..... ");
+// });
+
+// // Export the Express API
+// module.exports = app;
 
 var express = require("express");
 var jsonServer = require("json-server");
 var server = express();
 var cors = require("cors");
-const PORT = 4000;
-
+const port = 4000;
+// ...
 server.use(cors());
 // You may want to mount JSON Server on a specific end-point, for example /api
 // Optiona,l except if you want to have JSON Server defaults
 // server.use('/api', jsonServer.defaults());
 server.use("/api", jsonServer.router("db.json"));
 
-server.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
-
 server.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
+  res.send(`"Hello World!"`);
 });
 
-server.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
-
-// Export the Express API
-module.exports = app;
+module.exports = server;
