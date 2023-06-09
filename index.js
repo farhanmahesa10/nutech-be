b9afcf3508b1aa58f8754523f75376924167eaab
M Farhan <farhanmahesa10@gmail.com>
2023-06-09 21:24:07 +0700

    add api 3

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var jsonServer = require("json-server");
 var server = express();
 var cors = require("cors");
 const port = 4000;
@@ -8,7 +7,6 @@ server.use(cors());
 // You may want to mount JSON Server on a specific end-point, for example /api
 // Optiona,l except if you want to have JSON Server defaults
 // server.use('/api', jsonServer.defaults());
-server.use("/api", jsonServer.router("db.json"));
 
 server.get("/", (req, res) => {
   res.send(`"Hello World!"`);
