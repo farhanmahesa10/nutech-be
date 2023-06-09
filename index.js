1c1442db94eb3877fbe482c75ef8e2a0a442e326
M Farhan <farhanmahesa10@gmail.com>
2023-06-09 21:08:00 +0700

    add api 1

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
+// const express = require("express");
+
+// const app = express();
+// const PORT = 4000;
+
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hey this is my API running 🥳");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("This is my about route..... ");
+// });
+
+// // Export the Express API
+// module.exports = app;
+
 const express = require("express");
-var jsonServer = require("json-server");
+const jsonServer = require("json-server");
+
 const app = express();
-const PORT = 4000;
+const jsonRouter = jsonServer.router("db.json");
+const jsonMiddlewares = jsonServer.defaults();
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
+const PORT = process.env.PORT || 4000;
 
-app.use("/api", jsonServer.router("db.json"));
+// Middleware JSON Server
+app.use("/api", jsonMiddlewares);
 
+// Rute JSON Server
+app.use("/api", jsonRouter);
+
+// Rute Express.js
 app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
+  res.send("Selamat datang di server Express.js!");
 });
-
-app.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
-});
-
 // Export the Express API
 module.exports = app;
-
-// var express = require("express");
-// var jsonServer = require("json-server");
-// var server = express();
-// var cors = require("cors");
-// const port = 4000;
-// // ...
-// server.use(cors());
-// // You may want to mount JSON Server on a specific end-point, for example /api
-// // Optiona,l except if you want to have JSON Server defaults
-// // server.use('/api', jsonServer.defaults());
-// server.use("/api", jsonServer.router("db.json"));
-
-// server.get("/", (req, res) => {
-//   res.send(`"Hello World!"`);
-// });
-
-// server.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-// module.exports = server;
